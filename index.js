24a3d40a2844c2dd32dd4499b125c0c1924eb052
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-02-26 16:06:49 -0500

    tidy up the code

--- index.js
+++ index.js
@@ -13,30 +13,30 @@ const file = {
   ".png": "image/png",
 };
 
-http
-  .createServer(async (request, response) => {
-    const fileURL = url.parse(request.url);
-    const filePath =
-      fileURL.path === "/"
-        ? path.join("./public/", "index.html")
-        : path.join("./public/", fileURL.path);
+const server = http.createServer(async (request, response) => {
+  const fileURL = url.parse(request.url);
+  const filePath =
+    fileURL.path === "/"
+      ? path.join("./public/", "index.html")
+      : path.join("./public/", fileURL.path);
 
-    try {
-      const data = await fs.readFile(filePath);
-      response.writeHead(200, { "content-type": file[path.extname(filePath)] });
-      response.end(data);
-    } catch (err) {
-      if (err.code === "ENOENT") {
-        response.writeHead(404, "file not found", {
-          "content-type": "text/html",
-        });
-        const data = await fs.readFile("./public/404.html");
-        response.write(data);
-      }
-      console.log(err);
-      response.end();
+  try {
+    const data = await fs.readFile(filePath);
+    response.writeHead(200, { "content-type": file[path.extname(filePath)] });
+    response.end(data);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      response.writeHead(404, "file not found", {
+        "content-type": "text/html",
+      });
+      const data = await fs.readFile("./public/404.html");
+      response.write(data);
     }
-  })
-  .listen(8080, () => {
-    console.log("\n+++++++++++++++++++\nserver launched at port 8080");
-  });
+    console.log(err);
+    response.end();
+  }
+});
+
+server.listen(8080, () => {
+  console.log("\n+++++++++++++++++++\nserver launched at port 8080");
+});
